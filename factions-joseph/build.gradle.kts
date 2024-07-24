4fb302e62f5f316372559c5f9a3204bf9dff8ce2
Jake Moore <jakealexandermoore@icloud.com>
2024-07-24 03:55:17 -0700

    v2.1.6 -fix CI

--- factions-joseph/build.gradle.kts
+++ factions-joseph/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     // Unique dependencies for this module
     compileOnly(project(":common"))
     compileOnly("com.massivecraft:Factions-Joseph:2.13.7")
-    compileOnly("com.golfing8:FactionsKore:1.3.56.1")
+    compileOnly("com.golfing8:FactionsKore:1.3.62.1")
     compileOnly("com.massivecraft:MassiveCore:2.14.0")
 }
