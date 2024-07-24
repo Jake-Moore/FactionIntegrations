5f5bc9041f1a899ea2e5d66fb495e196e581585d
Jake Moore <jakealexandermoore@icloud.com>
2024-07-24 03:51:39 -0700

    v2.1.6 -fix CI

--- factions-jartex/build.gradle.kts
+++ factions-jartex/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     // Unique dependencies for this module
     compileOnly(project(":common"))
     compileOnly("com.massivecraft:Factions-Jartex:1.0")
-    compileOnly("net.jartex.factions:JartexFactions:1.0.0-SNAPSHOT")
+    compileOnly("net.jartex.factions:JartexFactions:1.0.0")
     compileOnly("net.craftimex.modules:MissionsModule:1.3.1")
 }
